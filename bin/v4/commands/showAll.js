8ddb1bcf74d2b66387452916650f439020537c08
keshavsoft <nknnkumar@yahoo.com>
2026-06-03 11:05:28 +0530

    4

---
+++ bin/v4/commands/showAll.js
@@ -0,0 +1,75 @@
+import { locateSource } from "./ShowAll/steps/locateSource.js";
+import { locateDestination } from "./ShowAll/steps/locateDestination.js";
+
+import { createProject } from "./ShowAll/steps/createProject.js";
+import { announce } from "./ShowAll/steps/announce.js";
+
+import resolveFolderName from "./ShowAll/steps/resolveFolderName.js";
+import checks from "./ShowAll/steps/checks.js";
+
+export default ({
+    folderName = "",
+    toPath = process.cwd(),
+    inAnnounce = true,
+    showLog = false
+}) => {
+    if (showLog) {
+        console.log("Resolving folder name...");
+    };
+
+    const resolvedFolderName = resolveFolderName({
+        name: folderName
+    });
+
+    if (showLog) {
+        console.log(`resolvedFolderName : ${resolvedFolderName}`);
+    };
+
+    const fromChecks = checks({
+        toPath,
+        inAnnounce,
+        inFolderName: resolvedFolderName,
+        showLog
+    });
+
+    if (fromChecks) return false;
+
+    if (showLog) {
+        console.log("Locating source...");
+    }
+
+    const source = locateSource({
+        showLog
+    });
+
+    if (showLog) {
+        console.log("Locating destination...");
+    }
+
+    const destination = locateDestination({
+        inResolvedFolderName: resolvedFolderName,
+        showLog
+    });
+
+    if (showLog) {
+        console.log("Creating project...");
+    }
+
+    createProject({
+        source,
+        destination,
+        showLog
+    });
+
+    if (inAnnounce) {
+
+        if (showLog) {
+            console.log("Announcing...");
+        }
+
+        announce({
+            inResolvedFolderName: resolvedFolderName,
+            showLog
+        });
+    }
+};
